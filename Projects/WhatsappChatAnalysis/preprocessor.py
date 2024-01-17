7b06cc09b393e6eb7a0e9d6bc75208abd97125ea
SK <sarthakkarandikar03@gmail.com>
2024-01-17 21:03:02 +0530

    changes

--- Projects/WhatsappChatAnalysis/preprocessor.py
+++ Projects/WhatsappChatAnalysis/preprocessor.py
@@ -1,18 +1,24 @@
 import re
 import pandas as pd
 
-def preprocess(data):
+def preprocess():
+    f = open("/content/WhatsApp Chat with Ojaswini.txt", "r", encoding = "utf-8")
+    data = f.read()
+
     pattern = "\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[APMapm]{2}\s-\s"
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
-    # for date in dates :
-    #     date.replace("\u202f", " ")
+    for date in dates :
+        date.replace("\u202f", " ")
 
     df = pd.DataFrame({"user_message" : messages, "message_date" : dates})
+    try:
+        df["message_date"] = pd.to_datetime(df["message_date"], format="%m/%d/%y, %I:%M %p - ")
+    except:
+        df["message_date"] = pd.to_datetime(df['message_date'], format="%d/%m/%Y, %H:%M - ")
 
-    df["message_date"] = pd.to_datetime(df["message_date"], format="%m/%d/%y, %I:%M %p - ")
     df.rename(columns = {"message_date" : "date"}, inplace = True)
 
     users = []
