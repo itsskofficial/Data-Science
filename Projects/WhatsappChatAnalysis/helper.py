b16117af8a6e2ab299bf8a3895004a96f177ef54
SK <sarthakkarandikar03@gmail.com>
2024-01-17 22:12:41 +0530

    file error

--- Projects/WhatsappChatAnalysis/helper.py
+++ Projects/WhatsappChatAnalysis/helper.py
@@ -60,7 +60,7 @@ def create_wordcloud(selected_user,df):
 
 def most_common_words(selected_user,df):
 
-    f = open("stop_hinglish.txt","r")
+    f = open("Projects/WhatsappChatAnalysis/stop_hinglish.txt","r")
     stop_words = f.read()
 
     if selected_user != "Overall":
